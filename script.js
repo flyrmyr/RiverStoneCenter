ea26a8152f8cab449b73fc5206a751a70c656cdc
Eric Meyer <Eric@Eric-Meyers-MacBook-Pro.local>
2016-01-27 02:07:13 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload = function(){
+  	var elements = document.getElementsByTagName("h1");
+  	for (var s=0; s<elements.length; s++){
+  		var strings = elements[s].innerHTML.split(' ');
+  		var replacement = '';
+
+		for (i = 0; i < strings.length; i++) {
+			if (strings[i] == '&amp;'){
+				replacement += '& ';
+			} else {
+				var end = strings[i].slice(1,strings[i].length);
+				console.log(strings[i][0]);
+			    replacement += ('<span class=first_letter>'+strings[i][0]+'</span>'+end+' ');
+			}
+		}
+		elements[s].innerHTML = replacement;
+  	}
+
+  var elements = document.getElementsByTagName("h2");
+  	for (var s=0; s<elements.length; s++){
+  		var strings = elements[s].innerHTML.split(' ');
+  		var replacement = '';
+
+		for (i = 0; i < strings.length; i++) {
+			if (strings[i] == '&amp;'){
+				replacement += '& ';
+			} else {
+				var end = strings[i].slice(1,strings[i].length);
+				console.log(strings[i][0]);
+			    replacement += ('<span class=first_letter>'+strings[i][0]+'</span>'+end+' ');
+			}
+		}
+		elements[s].innerHTML = replacement;
+  	}
+}
+
