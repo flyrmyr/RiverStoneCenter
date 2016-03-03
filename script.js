aa7e8a865dfedd3fa0c54e2f96a8b95ca7e6be38
emeyer161 <meyer.ej@gmail.com>
2016-03-03 02:41:31 -0500

    Double sub-nav and Google maps api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+$(document).ready(function(){
   	var elements = document.getElementsByTagName("h1");
   	for (var s=0; s<elements.length; s++){
   		var strings = elements[s].innerHTML.split(' ');
@@ -16,10 +16,10 @@ window.onload = function(){
 		elements[s].innerHTML = replacement;
   	}
 
-  var elements = document.getElementsByTagName("h2");
-  	for (var s=0; s<elements.length; s++){
-  		var strings = elements[s].innerHTML.split(' ');
-  		var replacement = '';
+	var elements = document.getElementsByTagName("h2");
+	for (var s=0; s<elements.length; s++){
+		var strings = elements[s].innerHTML.split(' ');
+		var replacement = '';
 
 		for (i = 0; i < strings.length; i++) {
 			if (strings[i] == '&amp;'){
@@ -27,10 +27,14 @@ window.onload = function(){
 			} else {
 				var end = strings[i].slice(1,strings[i].length);
 				console.log(strings[i][0]);
-			    replacement += ('<span class=first_letter>'+strings[i][0]+'</span>'+end+' ');
+				replacement += ('<span class=first_letter>'+strings[i][0]+'</span>'+end+' ');
 			}
 		}
-		elements[s].innerHTML = replacement;
-  	}
-}
+	elements[s].innerHTML = replacement;
+	}
+
+	$('.header').load("components/header.html");
+	$('.footer').load("components/footer.html");
+	$('.nav').load("components/nav.html");
+});
 
